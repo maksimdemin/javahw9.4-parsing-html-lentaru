9c48d6c1b8508cdf8630ad4b87efe3b1b55b2e70
MaksimDemin <mdeminalex@icloud.com>
2019-12-25 22:43:16 +0800

    'add try catch to class Main'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import com.deminmax.DownloadImages;
 import com.deminmax.ImageParser;
 import java.io.IOException;
+import java.net.UnknownHostException;
 
 public class Main {
 
@@ -8,6 +9,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        DownloadImages.downloadsImages(new ImageParser().parseHTMLFromURL(URL_LENTARU));
+        try {
+            DownloadImages.downloadsImages(new ImageParser().parseHTMLFromURL(URL_LENTARU));
+        } catch (UnknownHostException ex) {
+            ex.printStackTrace();
+        }
     }
 }
